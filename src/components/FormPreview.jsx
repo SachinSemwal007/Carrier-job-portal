d0ab23b82a82254417bc95e396d5cca1171038a2
SachinSemwal007 <sachinsemwal18@gmail.com>
2024-10-09 23:10:16 +0530

    done

--- src/components/FormPreview.jsx
+++ src/components/FormPreview.jsx
@@ -508,14 +508,14 @@ const FormPreview = ({
         >
           Download as PDF
         </Button>
-        {/* <Link
+        <Link
           href={certification}
           target="_blank"
           rel="noopener noreferrer"
           className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
           >
           Show Certificate
-        </Link> */}
+        </Link>
       </Modal.Footer>
     </Modal>
   );
