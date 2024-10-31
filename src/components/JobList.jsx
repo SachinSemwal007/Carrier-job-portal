f3308be6e73aa08df2e4409e6540cac83e62f19a
SachinSemwal007 <sachinsemwal18@gmail.com>
2024-11-01 01:42:24 +0530

    Done

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -34,7 +34,7 @@ const JobList = () => {
      
 
       <main className="flex-grow container mx-auto px-6 sm:px-8 py-10">
-        <h2 className="text-sm sm:text-4xl font-extrabold text-center text-gray-800 mb-10">
+        <h2 className="text-[22px] sm:text-4xl font-extrabold text-center text-gray-800 mb-10">
           Ongoing Hirings
         </h2>
 
